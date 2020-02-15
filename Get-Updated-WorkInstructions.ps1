b5f225f746ba30eafc60bf4fa9ace6daeb3c3148
lazysocks <tacomasocks@gmail.com>
2020-02-15 10:00:28 -0800

    Added path variables and date to csv filename

--- Get-Updated-WorkInstructions.ps1
+++ Get-Updated-WorkInstructions.ps1
@@ -1,5 +1,3 @@
-
-
 function Get-Updated-WorkInstructions {
 [CmdletBinding()]
 param (
@@ -13,11 +11,21 @@ param (
     Get-ChildItem *.doc* -Path $Path -Recurse  | Where-Object {!$_.psiscontainer} | Where-Object {$_.LastWriteTime -gt (Get-Date).AddDays(-$Days)}
 }
 
+#declare variables
 
-#Change Path and Days 
-$results = Get-Updated-WorkInstructions -Path . -Days 7 | Select-Object -Property name,LastWriteTime
-Add-Content -Path .\Updated-WorkInstructions.csv -Value '"Work Instruction Document Name", "Date Modified"'
+#Change Search Path to root folder to search
+$searchPath = "~"
+
+#Change days to number of days back you want to search for
+$Days = 7
+
+
+$date = Get-Date -Format "yyyyMMdd"
+$csvFileName = "{0}_Updated-WorkInstruction_Report.csv" -f $date
+
+$results = Get-Updated-WorkInstructions -Path $searchPath -Days $Days | Select-Object -Property name,LastWriteTime
+Add-Content -Path $PSScriptRoot\$csvFileName -Value '"Work Instruction Document Name", "Date Modified"'
 foreach($result in $results) { 
     $newline = "`"{0}`", `"{1}`"" -f $result.Name, $result.LastWriteTime.ToShortDateString()
-    Add-Content -Path .\Updated-WorkInstructions.csv -Value $newline
+    Add-Content -Path $PSScriptRoot\$csvFileName -Value $newline
 }
